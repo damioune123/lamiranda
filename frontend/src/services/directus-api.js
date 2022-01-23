e7cff1d58dd8e5d6c0c00a38cfb2185b561869ac
Damien Meur <dmeur@proxyclick.com>
2022-01-23 15:14:27 +0100

    fix eslint

--- frontend/src/services/directus-api.js
+++ frontend/src/services/directus-api.js
@@ -10,7 +10,6 @@ class DirectusAPI {
 
   // API REQUESTS
   getGalleries = async () => {
-    console.log('HEY GET GALLERIES');
     const { data } = await this.directusClient.getItems('galleries', {
       fields: 'id,name,gallery_items.*, gallery_items.image.data.*',
     });
